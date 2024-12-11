99ece0bf1e2381615dcdc9e67300e3ae94dc77b7
natscott1738 <natebaraka777@gmail.com>
2024-12-11 09:29:40 +0000

    files manager

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 function controllerRouting(app) {
   const router = express.Router();
@@ -15,6 +16,18 @@ function controllerRouting(app) {
   // should return the number of users and files in DB
   router.get('/stats', (req, res) => {
     AppController.getStats(req, res);
+  });
+
+  // User Controller
+
+  // should create a new user in DB
+  router.post('/users', (req, res) => {
+    UsersController.postNew(req, res);
+  });
+
+  // should retrieve the user base on the token used
+  router.get('/users/me', (req, res) => {
+    UsersController.getMe(req, res);
   });
 }
 
